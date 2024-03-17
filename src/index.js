39e4ea11d96edd94095e1ed1099c2435394551ab
Alej <alejandrocevedo09092002@gmail.com>
2024-03-16 20:47:53 -0400

    build

--- src/index.js
+++ src/index.js
@@ -8,10 +8,12 @@ const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
 const PostgresConnetion_1 = __importDefault(require("./config/PostgresConnetion"));
 const routes_1 = __importDefault(require("./routes"));
+const cors_1 = __importDefault(require("cors"));
 const app = (0, express_1.default)();
 const PORT = process.env.PORT || 3000;
 app.use(express_1.default.json());
 app.use(routes_1.default);
+app.use((0, cors_1.default)());
 app.get('/', (req, res) => {
     res.send('hello friend');
 });
